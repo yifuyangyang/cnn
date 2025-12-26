83ab0ab99c236948fc1ffcc347321a2520388cdd
lyf123456-wd <3502075687@qq.com>
2025-12-26 18:29:44 +0800

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -34,7 +34,7 @@ CONFIG = {
     "seed": 42,
 
     # 选择要训练的模型： "mlp" 或 "cnn"
-    "model": "mlp",
+    "model": "cnn",
 
     # 训练相关参数（可以改，用于观察收敛与精度变化）
     "epochs": 10,
@@ -44,7 +44,7 @@ CONFIG = {
 
     # 输出
     "save_plot": True,
-    "plot_path": "results.png",
+    "plot_path": "cnn.png",
 }
 
 
@@ -161,11 +161,14 @@ class MLP(nn.Module):
         # 你只需要改下面这些 Linear 的输入/输出维度即可。
         # ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
 
-        self.fc1 = nn.Linear(28 * 28, 256)   # 改这里：例如 128 / 256 / 512
-        self.fc2 = nn.Linear(256, 128)       # 改这里：例如 64 / 128 / 256
+        self.fc1 = nn.Linear(28 * 28, 512)   # 改这里：例如 128 / 256 / 512
+        self.fc2 = nn.Linear(512, 256)       # 改这里：例如 64 / 128 / 256
         # 如需增加第三个隐藏层，可新增 fc3，并把最后输出层改名
+        self.fc3 = nn.Linear(256, 128) 
         self.out = nn.Linear(128, 10)        # 最后一层输出固定 10 类（0~9）
 
+        self.drop = nn.Dropout(0.3)  # 30%的Dropout率
+        
         # 激活函数（通常用 ReLU）
         self.relu = nn.ReLU()
 
@@ -176,9 +179,11 @@ class MLP(nn.Module):
         x = x.view(x.size(0), -1)
 
         x = self.relu(self.fc1(x))
-        # x = self.drop(x)  # 若启用 Dropout
+        x = self.drop(x)  # 若启用 Dropout
         x = self.relu(self.fc2(x))
-        # x = self.drop(x)
+        x = self.drop(x)
+        x = self.relu(self.fc3(x))
+        x = self.drop(x)
         x = self.out(x)
         return x
 
@@ -222,8 +227,8 @@ class SimpleCNN(nn.Module):
         # 全连接层输入维度要写成： (conv2_out_channels * 7 * 7)
         # ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
 
-        c1_out = 16   # 改这里：8 / 16 / 32
-        c2_out = 32   # 改这里：16 / 32 / 64
+        c1_out = 32   # 改这里：8 / 16 / 32
+        c2_out = 64   # 改这里：16 / 32 / 64
 
         self.conv1 = nn.Conv2d(1, c1_out, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(c1_out, c2_out, kernel_size=3, padding=1)
@@ -232,8 +237,9 @@ class SimpleCNN(nn.Module):
         self.pool = nn.MaxPool2d(2)  # 2x2 池化，尺寸减半
 
         # 全连接层：输入是 c2_out * 7 * 7
-        self.fc1 = nn.Linear(c2_out * 7 * 7, 128)  # 可以改 128 -> 256 试试
-        self.fc2 = nn.Linear(128, 10)
+        self.fc1 = nn.Linear(c2_out * 7 * 7, 256)  # 可以改 128 -> 256 试试
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 10)
 
     def forward(self, x):
         # x: [B, 1, 28, 28]  (CNN 不需要 Flatten 输入)
